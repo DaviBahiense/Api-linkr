d4a7388e414ed6c79e968bd767a387a1ec747e52
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2022-03-25 15:50:45 -0300

    feat: like post

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -13,14 +13,52 @@ async function createPost(userId, description, link) {
 async function getPosts() {
   return connection.query(
     `
-    SELECT users.id AS "userId", users.name, users.img, link, description
+    SELECT posts.id AS id, users.id AS "userId", users.name, users.img, link, description
     FROM posts
     JOIN users ON users.id = posts."userId"
     ORDER BY posts.id DESC LIMIT 20`
   );
 }
 
+async function like(userId, postId){
+  return connection.query(`
+    INSERT
+      INTO likes ("usersId", "postsId")
+      VALUES ($1, $2)
+  `, [userId, postId])
+}
+
+async function unlike(userId, postId){
+  return connection.query(`
+    DELETE
+      FROM likes
+      WHERE "usersId"=$1 AND "postsId"=$2
+  `, [userId, postId])
+}
+
+async function checkPostExistence(id){
+  return connection.query(`
+  SELECT
+    *
+    FROM posts
+    WHERE id = $1
+`, [id]);
+}
+
+async function checkLikeExistence(userId, postId){
+  return connection.query(`
+  SELECT
+    *
+    FROM likes
+    WHERE "usersId" = $1 AND "postsId" = $2
+`, [userId, postId]);
+}
+
 export const postRepository = {
   createPost,
   getPosts,
+  like,
+  unlike,
+  checkPostExistence,
+  checkLikeExistence
 };
