ebf3f40ddde31a8e361620487e5b3f74ee263f27
DaviBahiense <davibnovaes@hotmail.com>
2022-03-26 04:53:19 -0300

    prepare deploy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express, { json } from 'express';
-import router from './routes/index.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { json } from "express";
+import router from "./routes/index.js";
 dotenv.config();
 
 const app = express();
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`)
-})
+  console.log(`Running on port ${PORT}`);
+});
