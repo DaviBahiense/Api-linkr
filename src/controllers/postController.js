b6829e82da84911770afda82147da170482013e0
Caroline <caroline.y.ldm@gmail.com>
2022-03-26 03:30:44 -0300

    delete created

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -47,3 +47,19 @@ export async function updatePost(req, res) {
     console.log(error);
   }
 }
+
+export async function deletePost(req, res) {
+  let { id } = req.params;
+
+  try {
+    const result = await postRepository.selectPost(id, res.locals.user.id);
+    if (result.rowCount === 0) {
+      return res.sendStatus(401);
+    }
+    await postRepository.deletePost(id);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
