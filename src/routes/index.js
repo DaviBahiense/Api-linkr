f2ad8dd1f697da113e8d50952d50b5ebe8a062c3
bielvbcg <gabrielvbcallegario@gmail.com>
2022-03-28 14:28:20 -0300

    add searchBarRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import userRouter from "./userRouter.js";
 import postRouter from "./postRouter.js";
 import tagsRouter from "./tagsRouter.js";
 import likeRouter from "./likeRouter.js";
+import searchBarRouter from "./searchBarRouter.js";
 
 const router = Router();
 router.use(authRouter);
@@ -11,5 +12,6 @@ router.use(userRouter);
 router.use(postRouter);
 router.use(likeRouter);
 router.use(tagsRouter);
+router.use(searchBarRouter)
 
 export default router;
