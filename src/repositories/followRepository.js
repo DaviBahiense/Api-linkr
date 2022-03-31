b923641a520b07fb3819b26ccd019498fdd357fd
DaviBahiense <davibnovaes@hotmail.com>
2022-03-31 03:52:10 -0300

    create getUserFollow

--- src/repositories/followRepository.js
+++ src/repositories/followRepository.js
@@ -1,41 +1,60 @@
-import {userRepository} from "./userRepository.js"
+import { userRepository } from "./userRepository.js";
 import { connection } from "../database.js";
 
 async function follow(userId, followId) {
-    return connection.query(
+  return connection.query(
     `
         INSERT
             INTO follows ("userId", "followId")
             VALUES ($1, $2)
-    `, [userId, followId]);
+    `,
+    [userId, followId]
+  );
 }
 
 async function unfollow(userId, followId) {
-    return connection.query(
+  return connection.query(
     `
         DELETE
             FROM follows
             WHERE "userId"=$1 AND "followId"=$2
-    `, [userId, followId]);
+    `,
+    [userId, followId]
+  );
 }
 
 async function checkUserExistence(id) {
-    return userRepository.usersId(id)
+  return userRepository.usersId(id);
 }
 
 async function checkFollowExistence(userId, followId) {
-    return connection.query(
+  return connection.query(
     `
         SELECT
             *
             FROM follows
             WHERE "userId" = $1 AND "followId" = $2
-    `, [userId, followId]);
+    `,
+    [userId, followId]
+  );
+}
+
+async function getUserFollow(userId) {
+  return connection.query(
+    `
+        SELECT
+            *
+        FROM follows
+        WHERE "userId" = $1 
+    `,
+    [userId]
+  );
 }
 
 export const followRepository = {
-    follow,
-    unfollow,
-    checkUserExistence,
-    checkFollowExistence
-}
+  follow,
+  unfollow,
+  checkUserExistence,
+  checkFollowExistence,
+  getUserFollow,
+};
--- src/routes/followRouter.js
+++ src/routes/followRouter.js
@@ -1,10 +1,25 @@
 import { Router } from "express";
-import { handleFollow, getFollow } from "../controllers/followController.js";
+import {
+  handleFollow,
+  getFollow,
+  getUserFollow,
+} from "../controllers/followController.js";
 import { checkUserExistence } from "../middlewares/checkUserExistence.js";
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 
-const followRouter = Router()
-followRouter.post('/follows/:id/:type', validateTokenMiddleware, checkUserExistence, handleFollow)
-followRouter.get('/follows/:id', validateTokenMiddleware, checkUserExistence, getFollow)
+const followRouter = Router();
+followRouter.post(
+  "/follows/:id/:type",
+  validateTokenMiddleware,
+  checkUserExistence,
+  handleFollow
+);
+followRouter.get(
+  "/follows/:id",
+  validateTokenMiddleware,
+  checkUserExistence,
+  getFollow
+);
+followRouter.get("/follows", validateTokenMiddleware, getUserFollow);
 
 export default followRouter;
