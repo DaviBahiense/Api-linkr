08d63a87b37a9983e7ada39a7bc78550a7ac8682
DaviBahiense <davibnovaes@hotmail.com>
2022-04-01 14:00:56 -0300

    change database tables

--- database-linkr/scripts/003-cria-posts.sql
+++ database-linkr/scripts/003-cria-posts.sql
@@ -3,6 +3,7 @@ CREATE TABLE "posts" (
 	"link" TEXT NOT NULL,
 	"description" TEXT,
 	"userId" integer NOT NULL REFERENCES users(id),
+	"time" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
 	"metadataImg" TEXT NOT NULL,
 	"metadataDescription" TEXT NOT NULL,
 	"metadataTitle" TEXT NOT NULL
